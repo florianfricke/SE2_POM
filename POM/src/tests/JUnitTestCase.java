d34060904e0cbecf1981dc3e786f1b0e7bf8112e
oksana.tsurkan <oksana.tsurkan97@gmail.com>
2017-06-15 02:03:50 +0200

    Test main functions

--- POM/src/tests/JUnitTestCase.java
+++ POM/src/tests/JUnitTestCase.java
@@ -1,11 +1,23 @@
 package tests;
 
+import pom_db_interface.*;
+
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import mes_db_interface.*;
 import pom_db_interface.*;
 import psql_mes_db.MesDbService;
@@ -14,92 +26,363 @@ import types.*;
 
 public class JUnitTestCase {
 	
-	/*Functions to test:
-	public boolean addCustomer(Customer cust);
+	private Connection con = null;
+	PreparedStatement stmt = null;
+
+	public JUnitTestCase(){
+		openConnection();
+	}
+	
+	/*functions to test:
+	public boolean addCustomer(Customer cust); +
 	public boolean updateCustomer(Customer cust);
 	public List<Customer> getCustomerList();
-	public boolean deleteCustomer(String id);
+	public boolean deleteCustomer(String id); +
 	public List<Order> getOrderList();
 	public List<Address> getAddressList(String custId);
 	public List<Contact> getContactList(String custId);
 	public List<BankAccount> getBankAccountList(String custId);
-	public boolean addOrder(Order order);
-	boolean deleteOrder(String orderno);
+	public boolean addOrder(Order order); +!return false anstatt von true!
+	public boolean updateOrder(Order order);
+	boolean deleteOrder(String orderno);+
+	public int getDayCapacity();
 	public Customer getCustomer(String customerId);
+	
+	releaseOrder
 	*/
+
+	//function for creating a test customer
+	private Customer addTestCustomer(){
+		IPomDbService pomPersistance = new PomDbService();
+		Customer testCustomer = new Customer("","Test customer","A","Here is test customer");
+		pomPersistance.addCustomer(testCustomer);
+		String testCustomerID = "";
+		PreparedStatement stmt = null;
+		ResultSet rs=null;
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Customer WHERE companyname='Test customer'");
+			rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   testCustomerID =rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//insert one testCustomer's address
+		Address testCustomerAddress = new Address(	"13", 
+													"Hochschulstr.", 
+													"32", 
+													"01069", 
+													"Dresden", 
+													"Gernamy", 
+													false);
+		
+		ObservableList<Address> addressList = FXCollections.observableArrayList();
+		//List<Address> addressList=new ArrayList<Address>();
+		addressList.add(testCustomerAddress);
+		testCustomer.setAddressList((ObservableList<Address>) addressList);
+		//insert one testCustomer's contact
+		Contact testCustomerContact = new Contact(	"13", 
+													"Name", 
+													"First name", 
+													"personal", 
+													"", 
+													"");
+		ObservableList<Contact> contactList = FXCollections.observableArrayList();
+		//List<Contact> contactList=new ArrayList<Contact>();
+		contactList.add(testCustomerContact);
+		testCustomer.setContactList((ObservableList<Contact>) contactList);
+		//insert one testCustomer's bank account
+		BankAccount testCustomerBankAccount = new BankAccount (	"13",
+																"IBAN", 
+																"BIC", 
+																"BankName");
+		ObservableList<BankAccount> bankAccountList = FXCollections.observableArrayList();
+		//List<BankAccount> bankAccountList=new ArrayList<BankAccount>();
+		bankAccountList.add(testCustomerBankAccount);
+		testCustomer.setBankAccountList((ObservableList<BankAccount>) bankAccountList);	
+		
+		
+		return testCustomer;
+	}
+	
 	
 
 	/***************************************************************/
 	/*                        addCustomer()                        */
-	//Customer nichts eingegeben
-	@Test
-	public void testAddCustomerNoParameter(){
+	//Standard case
+	//@Test
+	public void testAddCustomer(){
 		IPomDbService pomPersistance = new PomDbService();
-		Customer testCustomer = new Customer();
-		boolean result = pomPersistance.addCustomer(testCustomer);
-		assertEquals(false,result);
+		Customer testCustomer = new Customer("","Test customer","A","Here is test customer");
+		boolean expecting = true;
+		boolean result=pomPersistance.addCustomer(testCustomer);
+		assertEquals(expecting,result);
 	}
-	//Customer Name nicht eingegeben
-	@Test
-	public void testAddCustomerNoName(){
-		IPomDbService pomPersistance = new PomDbService();
-		Customer testCustomer = new Customer("12345","","A","comment");
-		boolean result = pomPersistance.addCustomer(testCustomer);
-		assertEquals(false,result);
-	}
-	//Customer Ranking nicht eingegeben
-	@Test
-	public void testAddCustomerNoRanking(){
-		IPomDbService pomPersistance = new PomDbService();
-		Customer testCustomer = new Customer("12345","Name","","comment");
-		boolean result = pomPersistance.addCustomer(testCustomer);
-		assertEquals(false,result);
-	}
-	//Customer Ranking nicht im Wertebereich
-	@Test
-	public void testAddCustomerFalseRanking(){
-		IPomDbService pomPersistance = new PomDbService();
-		Customer testCustomer = new Customer("12345","Name","abc","comment");
-		boolean result = pomPersistance.addCustomer(testCustomer);
-		assertEquals(false,result);
-	}
-	/***************************************************************/
 	
-	
-	/***************************************************************/
+	//Prove if customer data are inserted to DB 
+	//@Test
+	public void dataTestAddCustomer(){
+			Customer testCustomer = addTestCustomer();
+			IPomDbService pomPersistance = new PomDbService();		
+			String nameExpected = testCustomer.toString();
+			String nameReal="";
+			try {
+				stmt = con.prepareStatement("SELECT * FROM customer Where companyname ='"+nameExpected+"'");
+				ResultSet rs = stmt.executeQuery();
+				while (rs.next())
+				{
+				nameReal = rs.getString("companyname");	
+				}
+				rs.close();
+			    stmt.close();		
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			assertEquals(nameExpected,nameReal);
+	}	
+		
+	/***************************************************************/	
 	/*                        deleteCustomer()                     */
-	//Beim Löschen des Kundes kein ID eingegeben
-	@Test
+	//Standard
+	//@Test
 	public void testDeleteCustomer(){
 		IPomDbService pomPersistance = new PomDbService();
-		boolean result = pomPersistance.deleteCustomer(null);
-		assertEquals(false,result);
+		Customer testCustomer = new Customer("","CustromerToDelete","A","Für deleteCustomer() test");
+		pomPersistance.addCustomer(testCustomer);
+		PreparedStatement stmt = null;
+		String CustomerToDeleteID = "";
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Customer WHERE companyname='CustromerToDelete'");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   CustomerToDeleteID =rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		boolean result = pomPersistance.deleteCustomer(CustomerToDeleteID);
+		assertEquals(true,result);
 	}
 	
-	//Kunde hat noch offene Fertigungsaufträge, state=...
-	//@Test DEAKTIVIERT
+	//Prove if there are still data in the DB
+	//@Test
+	public void dataTestDeleteCustomer(){
+		IPomDbService pomPersistance = new PomDbService();
+		Customer testCustomer = new Customer("","CustromerToDeleteData","A","Für deleteCustomer() test");
+		pomPersistance.addCustomer(testCustomer);
+		PreparedStatement stmt = null;
+		String CustomerToDeleteID = "";
+		ResultSet rs=null;
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Customer WHERE companyname='CustromerToDeleteData'");
+			rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   CustomerToDeleteID =rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		pomPersistance.deleteCustomer(CustomerToDeleteID);
+		
+		//really deleted from the database?
+		ResultSet realResult=null;
+		String result="";
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Customer WHERE companyname='CustromerToDeleteData'");
+			realResult = stmt.executeQuery();
+			
+			while (realResult.next())
+			{
+			   result=realResult.getString("id");
+			}
+			realResult.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//We are expecting no data in the DB
+		String expresult ="";
+		assertEquals(result,expresult);
+	}
+	
+	//Trying to delete customer with existing orders 
+	//!Before running this test, create new customer with name "Test" and insert all data(address,contact,bank)
+	//@Test
 	public void testDeleteCustomerWithOrders(){
 		IPomDbService pomPersistance = new PomDbService();
-		Customer testCustomer = new Customer("00013","Test Löschen", "A","");
-		pomPersistance.addCustomer(testCustomer);
-		Order testOrder = new Order("10000","00013","","","",0,0,"PLANNED","","2017-06-11","","","","",10,0,"");
+		PreparedStatement stmt = null;
+		String customerToDeleteID = "";
+		ResultSet rs=null;
+		//get customerID
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Customer WHERE companyname='Test'");
+			rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   customerToDeleteID =rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//get addressID
+		String addressID="";
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Address WHERE customerid='"+customerToDeleteID+"'");
+			rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   addressID =rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//get contactID
+		String contactID="";
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Contact WHERE customerid='"+customerToDeleteID+"'");
+			rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   contactID =rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+				
+		Order testOrder = new Order("",customerToDeleteID,addressID,contactID,"",0,0,State.PLANNED.toString(),"",LocalDate.now(),LocalDate.now(),LocalDate.now(),LocalDate.now(),LocalDate.now(),LocalDate.now(),10,0,"");
+		
 		pomPersistance.addOrder(testOrder);
-		boolean result = pomPersistance.deleteCustomer("00013");
+		
+		boolean result = pomPersistance.deleteCustomer(customerToDeleteID);
 		assertEquals(false,result);
 	}
-	/***************************************************************/
 	
 	/***************************************************************/
-	/*                        deleteOrder()                     */
-	@Test
-	//Beim Löschen der Bestellung kein ID eingegeben
+	/*                           addOrder()                        */
+	//Standard
+	//@Test
+	public void testAddOrder(){
+		IPomDbService pomPersistance = new PomDbService();
+		PreparedStatement stmt = null;
+		String customerID = "";
+		ResultSet rs=null;
+		//get customerID
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Customer WHERE companyname='Test'");
+			rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   customerID =rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//get addressID
+		String addressID="";
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Address WHERE customerid='"+customerID+"'");
+			rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   addressID =rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//get contactID
+		String contactID="";
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Contact WHERE customerid='"+customerID+"'");
+			rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   contactID =rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+				
+		Order testOrder = new Order("",customerID,addressID,contactID,"",0,0,State.PLANNED.toString(),"",LocalDate.now(),LocalDate.now(),LocalDate.now(),LocalDate.now(),LocalDate.now(),LocalDate.now(),10,0,"");
+		
+		boolean result = pomPersistance.addOrder(testOrder);
+		
+		assertEquals(true,result);
+	
+	}
+		
+	/***************************************************************/
+	/*                        deleteOrder()                        */
+	//Standard
+	//@Test
 	public void testDeleteOrder(){
 		IPomDbService pomPersistance = new PomDbService();
-		boolean result = pomPersistance.deleteOrder(null);
-		assertEquals(false,result);
+		Customer testCustomer = addTestCustomer();
+		String custID=testCustomer.idProperty().toString();
+		String orderID = "";
+		PreparedStatement stmt = null;
+		try {
+			stmt = con.prepareStatement("SELECT * FROM Order WHERE customerid='"+custID+"'");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next())
+			{
+			   orderID=rs.getString("id");
+			}
+			rs.close();
+		    stmt.close();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		boolean result = pomPersistance.deleteOrder(orderID);
+		assertEquals(true,result);
 	}
 	/***************************************************************/
-
 	
-
+	
+	
+	private boolean openConnection(){
+		try{
+			 Class.forName("org.postgresql.Driver");
+	         con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/pom","postgres", "0815");
+	         return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			System.err.println(e.getClass().getName()+": "+e.getMessage());
+		}
+		System.out.println("Erfolgreich verbunden!");
+		return false;
+	}
 }
